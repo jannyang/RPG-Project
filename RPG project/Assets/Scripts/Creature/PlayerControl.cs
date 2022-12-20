879ebb59ce6ef207df7ab92274f141b9eafb8097
jannyang <wlghks1725@naver.com>
2022-12-20 20:46:22 +0900

    2022_12_20_Update

--- RPG project/Assets/Scripts/Creature/PlayerControl.cs
+++ RPG project/Assets/Scripts/Creature/PlayerControl.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Player))]
 [DisallowMultipleComponent]     // 한 오브젝트에 하나만 달리게
 public class PlayerControl : MonoBehaviour
 {
-
+    
     Player player;
     private IdleEvent idleEvent;
     float moveSpeed = 5;
+
+    [SerializeField]
+    private InputActionReference movement;
 
     void Awake()
     {
@@ -22,12 +26,15 @@ public class PlayerControl : MonoBehaviour
         MovementInput();
     }
 
+   
     private void MovementInput()
     {
-        float horizontalMovement = Input.GetAxisRaw("Horizontal");
-        float verticalMovement = Input.GetAxisRaw("Vertical");
-        Vector2 direction = new Vector2(horizontalMovement, verticalMovement);
+        //float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        //float verticalMovement = Input.GetAxisRaw("Vertical");
+        //Vector2 direction = new Vector2(horizontalMovement, verticalMovement);
 
+        Vector2 direction = movement.action.ReadValue<Vector2>();
+        Debug.Log(direction);
         if (direction != Vector2.zero)
         {
             player.MovementEvent.CallMovementEvent(direction, moveSpeed);
